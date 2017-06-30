6dc0f5f38a1bcdc467641609c7bf26290311add5
Jérôme Roux <jerome.roux@pole-emploi.fr>
2017-06-30 17:50:07 +0200

    chore(erreur): utilise la gestion des erreurs par défaut

--- app.js
+++ app.js
@@ -22,11 +22,4 @@ app.use(function (req, res, next) {
     next(err);
 });
 
-// error handler
-app.use(function (err, req, res, next) {
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
-    res.status(err.status || 500).send(err.message);
-});
-
 module.exports = app;
