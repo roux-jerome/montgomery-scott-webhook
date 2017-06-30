9d19af9eae42b6eeae9f7a8f3a66529c68fa316b
Jérôme Roux <jerome.roux@pole-emploi.fr>
2017-06-30 18:02:01 +0200

    feat(securite): ajoute helmet

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 var express = require('express');
-var path = require('path');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var helmet = require('helmet');
 
 var webhook = require('./routes/webhook');
 
 var app = express();
 
 app.use(logger('dev'));
+app.use(helmet());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
